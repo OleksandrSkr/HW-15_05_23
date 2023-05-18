de7a934ae3ab99ebddfcb3be8f660804252c93f7
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-05-18 23:17:58 +0300

    added venv

---
+++ app.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+import time
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://medium.com/")
